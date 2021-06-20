0ffe3c1bdb900a4497d7a63efffa213814448e3e
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-19 22:04:48 -0400

    add : Welcome statement : .

---
+++ UserRegistration.java
@@ -0,0 +1,24 @@
+/***********************************************************************************************
+ * @purpose Implementing User Registration Program Using REGEX. 
+ * @author Rekha
+ *@version 1.0
+ *@since 18-06-2021
+ ************************************************************************************************/
+
+package com;
+
+/**
+ * UserRegistration is a class of public type
+ * printing Welcome statement
+ * @author rekha
+ *
+ */
+
+public class UserRegistration {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to User Registration Program");
+	}
+
+}
